820b5e6d7f8b5532af5bbecd7417a22ba58fc79a
syhan7516 <syhan7516@naver.com>
2022-08-06 22:01:28 +0900

    HTML 원하는 요소를 찾은 후 원하는 정보 딕셔너리에 추가 완료

--- scrap.py
+++ scrap.py
@@ -1,7 +1,11 @@
 # reuqests 모듈 import
 import requests
+
 # bs4 모듈 import
 import bs4
+
+# 뉴스 저장 객체
+news = {}
 
 # crawling 하고자하는 url 지정
 crawling_url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=news&query=%EA%B8%88%EC%98%A4%EA%B3%B5%EA%B3%BC%EB%8C%80%ED%95%99%EA%B5%90&oquery=%EA%B8%88%EC%98%A4%EA%B3%B5%EA%B3%BC%EB%8C%80%ED%95%99%EA%B5%90&tqi=hXyBYwp0JywssC7KGosssssstFR-185396&nso=so%3Add%2Cp%3Aall&mynews=0&office_section_code=0&office_type=0&pd=0&photo=0&sort=1"
@@ -12,4 +16,17 @@ response = requests.get(crawling_url)
 # 전달받은 응답 값을 bs4 라이브러리를 통해 html로 파싱하기
 result_html = bs4.BeautifulSoup(response.text,'html.parser')
 
-print(result_html)
+# HTML 원하는 요소 찾기 (반환 : 여러가지)
+contents_title = result_html.find_all("a",{"class":"news_tit"})
+contents_company = result_html.find_all("a",{"class":"info press"})
+contents_date = result_html.find_all("span",{"class":"info"})
+
+# 원하는 정보 딕셔너리로 추가하기
+for content in range(0,10):
+    news[str(content)] = {} 
+    news[str(content)]["date"]= contents_date[content].get_text()
+    news[str(content)]["company"] = contents_company[content].get_text()
+    news[str(content)]["title"] = contents_title[content]["title"]
+
+for new in news:
+    print(news[new])
